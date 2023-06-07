97c4fd4f23e29b46c452203b9adfae9f8e0034b1
ifchuga <qpalzm1056ty@gmail.com>
2023-06-07 15:22:43 +0900

    for_test

---
+++ Assets/New Folder/player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public int health;
+
+    public int damage;
+
+    public void Attack()
+    {
+        Debug.Log("Attack");
+    }
+}
